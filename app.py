10723718c073ef3b7dfa44438245ddbf5af6be6c
Teik <teik.lee@capgemini.com>
2024-07-11 15:55:54 -0400

    updated for team

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from agents.self_query_rag_agent import SelfQueryRagAgent
-from agents.vectordb_rag_agent import VectorDbRagAgent
-
-
 if __name__ == "__main__":
     # agent = CandidateAgent()
     # agent.initialize_no_resume()
-    
+
     # while True:
     #     user_input = input(">> ")
     #     if user_input.lower() == "bye":
@@ -13,24 +9,6 @@ if __name__ == "__main__":
     #         break
 
     #     if user_input is not None:
-    #         resp = agent.chat(user_input)
-    #         print(resp)
-
-    # agent = RecruiterAgent()
-
-    # job_titles = list(map(lambda x: x.title, jobs))
-    # print(job_titles)
-
-    # agent = SelfQueryRagAgent()
-    agent = VectorDbRagAgent()
-
-    while True:
-        user_input = input(">> ")
-        if user_input.lower() == "bye":
-            print("LLM: Goodbye")
-            break
-
-        if user_input is not None:
-            # resp = agent.chat(user_input)
-            print(agent.run_small(user_input))
-            print(agent.run_large(user_input))
+    #         # resp = agent.chat(user_input)
+    #         print(agent.run_small(user_input))
+    #         print(agent.run_large(user_input))
